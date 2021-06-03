ebed4f1d1979c14a3d9f1a416f06c5b3a176b55c
Kaamya.K <56751950+xxxKaamyaxxx@users.noreply.github.com>
2021-06-03 21:58:43 +0530

    Add files via upload
    Binary stairs / Pattern Printing / Nested Loops

---
+++ binary-stairs.c
@@ -0,0 +1,26 @@
+/*Print the pattern for no. of stairs given.
+ex: for 6:
+01
+0101
+010101
+01010101
+0101010101
+010101010101 */
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("\nEnter no. of stairs: ");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<=i; j++)
+            printf("01");
+        printf("\n");
+    }
+
+    return 0;
+}
